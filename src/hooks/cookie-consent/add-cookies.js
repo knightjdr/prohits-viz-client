94c226419959d416db168b8c43432c943366e927
James Knight <knightjdr@gmail.com>
2020-12-15 09:24:55 -0500

    Refactor consent cookie logic

--- src/hooks/cookie-consent/add-cookies.js
+++ src/hooks/cookie-consent/add-cookies.js
@@ -1,22 +1,22 @@
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
 const addCookies = () => {
   if (process.env.NODE_ENV === 'production') {
-    window[`ga-disable-${process.env.REACT_APP_GOOGLE_ANALYTICS}`] = false;
-    /* eslint-disable */
-    (function (i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r;
-      i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date();
-      a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-    ga('create', process.env.REACT_APP_GOOGLE_ANALYTICS, 'auto');
-    ga('set', 'anonymizeIp', true);
-    ga('send', 'pageview');
-    /* eslint-enable */
+    ReactGA.initialize(
+      process.env.REACT_APP_GOOGLE_ANALYTICS,
+      {
+        gaOptions: {
+          anonymizeIp: true,
+        },
+      },
+    );
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+    const history = createBrowserHistory();
+    history.listen((location) => {
+      ReactGA.pageview(location.pathname + location.search);
+    });
   }
 };
 
--- src/hooks/cookie-consent/remove-cookies.js
+++ src/hooks/cookie-consent/remove-cookies.js
@@ -1,9 +1,11 @@
 const revokeConsent = () => {
-  window[`ga-disable-${process.env.REACT_APP_GOOGLE_ANALYTICS}`] = true;
-  const host = window.location.hostname;
-  document.cookie = `_ga=; domain=.${host}; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
-  document.cookie = `_gat=; domain=.${host}; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
-  document.cookie = `_gid=; domain=.${host}; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
+  if (process.env.NODE_ENV === 'production') {
+    window[`ga-disable-${process.env.REACT_APP_GOOGLE_ANALYTICS}`] = true;
+    const host = window.location.hostname;
+    document.cookie = `_ga=; domain=.${host}; path=/; sameSite=strict; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
+    document.cookie = `_gat=; domain=.${host}; path=/; sameSite=strict; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
+    document.cookie = `_gid=; domain=.${host}; path=/; sameSite=strict; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
+  }
 };
 
 export default revokeConsent;
